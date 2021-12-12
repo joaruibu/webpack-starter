1a0e2126832b6e00772caea835e817d20cb8e6cc
Joaquín <joaquin.design@gmail.com>
2021-12-12 17:22:19 +0100

    Primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,15 @@
+import '../css/componentes.css';
+// import archijobs from '../assets/img/Archi.png';
+export const saludar = nombre => {
+    console.log('Creando etiqueta H1');
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola${nombre}!-!`;
+    document.body.append(h1);
+
+
+    //Img
+    // console.log(archijobs);
+    // const img = document.createElement('img');
+    // img.src = archijobs;
+    // document.body.append(img)
+}
